dfdc0203d503ae5985e2f84d1251646926458c9a
tongliu <tongliucn@gmail.com>
2017-11-27 16:11:07 -0500

    add heapsort and comment out bubblesort

--- simulator/distributor.c
+++ simulator/distributor.c
@@ -11,6 +11,47 @@ static Distributor dist;
 
 typedef unsigned long ULONG;
 
+// add the heapsort function
+void Swap( int *num_a, int *num_b )
+{
+    int temp = *num_b;
+    *num_b = *num_a;
+    *num_a = temp;
+}
+
+void HeapAdjust(int array[], int i, int nLength)
+{
+    int nChild, nTemp;
+    for (nTemp = array[i]; 2 * i + 1 < nLength; i = nChild)
+    {
+        nChild = 2 * i + 1;
+        if (nChild != nLength - 1 && array[nChild + 1] > array[nChild])
+            ++nChild;
+        if (nTemp < array[nChild])
+        {
+            array[i] = array[nChild];
+        }
+        else         {
+            break;
+        }
+    }
+    array[i] = nTemp;
+}
+void HeapSort(int array[], int length)
+{
+    printf("heapsorting......");
+    for (int i = length / 2 - 1; i >= 0; --i)
+    {
+        HeapAdjust(array, i, length);
+    }
+    for (int i = length - 1; i > 0; --i)
+    {
+        Swap(&array[0], &array[i]);
+        HeapAdjust(array, 0, i);
+    }
+}
+
+
 
 static inline ULONG b(int n, int k){
 	    if(n < k) return 0;
@@ -155,7 +196,7 @@ static void RADBased(Segment** sbuf, int num,int did){
 	printf("now we are in RAD mode...\n");
 	placement = BASLINE(b(ARRAY_SIZE,STRIPE_SIZE)*STRIPE_SIZE*STRIPE_SIZE/ARRAY_SIZE,ARRAY_SIZE,dist._f[did],dist._fSize[did],dist._DArray[did],dist._RArray[did], dist._sizeofR[did],STRIPE_SIZE-CODE_SIZE,CODE_SIZE);
 
-	int p, q;
+/*	int p, q;
 	Segment* t;
 		for(p =0; p < (num-1); p++)
 		{
@@ -168,6 +209,14 @@ static void RADBased(Segment** sbuf, int num,int did){
 	                sbuf[q+1]=t;  
 	            }  
 		}
+*/
+	int arrayref[num];
+	for(int i = 0; i< num; ++i)
+	{
+		arrayref[i] = dist._sen[sbuf[i]->id].ref;
+	}
+
+	HeapSort(arrayref, num);
 
 	for (i=0;i<dist._sizeofR[did];i++)
 		dist._CDArray[did][dist._RArray[did][i].ab_file_id*ARRAY_SIZE+placement[dist._RArray[did][i].block_id].node_id]++;
